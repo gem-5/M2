e4639adc14533fac446a33ac1c62f61283bd8ad7
jp94 <jamespark@gatech.edu>
2014-09-04 00:34:51 -0400

    Formatted Code in Person3.java

--- src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -1,48 +1,46 @@
 package edu.gatech.oad.antlab.person;
+
 /**
- *  A simple class for person 3
- *  returns their name and a
- *  reversed string 
- *  
- *  @author  Bob
- *  @version 1.1
+ * A simple class for person 3 returns their name and a reversed string
+ * 
+ * @author Bob
+ * @version 1.1
  */
 public class Person3 {
-   /** Holds the persons real name */  
+	/** Holds the persons real name */
 	private String name;
-	
+
 	/**
-	 * The constructor, takes in the persons
-	 * name
-	 * @param pname the person's real name
+	 * The constructor, takes in the persons name
+	 * 
+	 * @param pname
+	 *            the person's real name
 	 */
-	public Person3(String pname){
-	  name = pname;
+	public Person3(String pname) {
+		name = pname;
 	}
-	
+
 	/**
-	 * Return a string rep of this object
-	 * that varies with an input string
+	 * Return a string rep of this object that varies with an input string
 	 *
-	 * @param input the varying string
-	 * @return the string representing the 
-	 *         object
+	 * @param input
+	 *            the varying string
+	 * @return the string representing the object
 	 */
 	public String toString(String input) {
-	  return name + calc(input);
+		return name + calc(input);
 	}
-	
+
 	/**
-	 * This method should take the string
-	 * input and return its reverse.
-	 * given "gtg123b" it should return
-	 * b321gtg.
+	 * This method should take the string input and return its reverse. given
+	 * "gtg123b" it should return b321gtg.
 	 *
-	 * @param input the string to be reversed
+	 * @param input
+	 *            the string to be reversed
 	 * @return the reversed string
 	 */
 	private String calc(String input) {
-	  //Person 3 put your implementation here
-	  return null;
+		// Person 3 put your implementation here
+		return null;
 	}
 }
