105b3440dbb547de9d40d5fb9b0055f3fe795150
aLiu4 <alex.liu004@gmail.com>
2014-09-05 16:00:51 -0400

    Update Person5.java
    Completed calc method

--- src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Alex Liu
  *  @version 1.1
  */
 public class Person5 {
@@ -30,8 +30,11 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+		if (input.length() <= 2) {
+			return input;
+		}
+		String rotated = input.substring(2) + input.substring(0, 2);
+		return rotated;
 	}
 	
 	/**
